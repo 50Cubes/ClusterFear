371c4e180d88532632cd9901a0087d031f367d80
Steazy <stichk@gmail.com>
2012-09-14 11:32:03 -0700

    excellent rotation

--- Kobold2D-2.0.3/ClusterFear/Projectfiles/GameNodes/SGMover.m
+++ Kobold2D-2.0.3/ClusterFear/Projectfiles/GameNodes/SGMover.m
@@ -17,10 +17,51 @@
 
 @implementation SGMover
 
--(void)setPosition:(CGPoint)position
+//-(void)setPosition:(CGPoint)position
+//{
+//    float xDirection = position.x;
+//    float yDirection = position.y;
+//    
+//    xDirection -= position_.x;
+//    yDirection -= position_.y;
+//    
+//    if( isnan(xDirection) || isnan(yDirection) )
+//        NSLog(@"Invalid position");
+//    
+//    float magnitude = sqrtf(xDirection * xDirection + yDirection * yDirection);
+//    
+//    xDirection /= magnitude;
+//    yDirection /= magnitude;
+//    
+//    float rotation = atan2f(yDirection,-xDirection);
+//    
+//    CCLOG(@"rotation is: %f with yDIrection %f xDir %f", rotation, xDirection, yDirection);
+//    
+////    [super setPosition:position];
+//    
+//    rotation = CC_RADIANS_TO_DEGREES(rotation) - 90.0f;
+//    if( rotation != rotation_ )
+//    {
+//        [self setRotation:rotation];
+////        NSLog(@"Rotated to %f degress with x: %f y: %f", CC_RADIANS_TO_DEGREES(rotation), xDirection, yDirection);
+//    }
+//}
+
+-(void)getHitFromWeapon:(SGWeapon *)weapon{
+    health -= [weapon damageInflicted];
+    if(health <= 0){
+        [self die];
+    }
+}
+
+-(void)die{
+    
+}
+
+-(void)moveToPoint:(CGPoint)targetPoint
 {
-    float xDirection = position.x;
-    float yDirection = position.y;
+    float xDirection = targetPoint.x;
+    float yDirection = targetPoint.y;
     
     xDirection -= position_.x;
     yDirection -= position_.y;
@@ -37,29 +78,15 @@
     
     CCLOG(@"rotation is: %f with yDIrection %f xDir %f", rotation, xDirection, yDirection);
     
-    [super setPosition:position];
+    //    [super setPosition:position];
     
     rotation = CC_RADIANS_TO_DEGREES(rotation) - 90.0f;
     if( rotation != rotation_ )
     {
         [self setRotation:rotation];
-//        NSLog(@"Rotated to %f degress with x: %f y: %f", CC_RADIANS_TO_DEGREES(rotation), xDirection, yDirection);
+        //        NSLog(@"Rotated to %f degress with x: %f y: %f", CC_RADIANS_TO_DEGREES(rotation), xDirection, yDirection);
     }
-}
-
--(void)getHitFromWeapon:(SGWeapon *)weapon{
-    health -= [weapon damageInflicted];
-    if(health <= 0){
-        [self die];
-    }
-}
-
--(void)die{
     
-}
-
--(void)moveToPoint:(CGPoint)targetPoint
-{
     [self runAction:[CCMoveTo actionWithDuration:0.5f position:targetPoint]];
 }
 
