946d20294354d2296d84a1e2084576ced4673e31
Alan Richardson <85@50cubes.com>
2012-09-14 14:55:40 -0700

    BatCluster

---
+++ Kobold2D-2.0.3/ClusterFear/Projectfiles/GameNodes/SGBatCluster.h
@@ -0,0 +1,13 @@
+//
+//  SGBatCuster.h
+//  ClusterFear
+//
+//  Created by Katherine Geiger on 9/14/12.
+//
+//
+
+#import "FoeCluster.h"
+
+@interface SGBatCuster : FoeCluster
+
+@end
---
+++ Kobold2D-2.0.3/ClusterFear/Projectfiles/GameNodes/SGBatCluster.m
@@ -0,0 +1,13 @@
+//
+//  SGBatCuster.m
+//  ClusterFear
+//
+//  Created by Katherine Geiger on 9/14/12.
+//
+//
+
+#import "SGBatCuster.h"
+
+@implementation SGBatCuster
+
+@end
