551c757493c661e3c2bd73cfa7e15480f3b2ffa2
Steazy <stichk@gmail.com>
2012-09-14 11:24:36 -0700

    better rotation

--- Kobold2D-2.0.3/ClusterFear/Projectfiles/GameNodes/SGMover.m
+++ Kobold2D-2.0.3/ClusterFear/Projectfiles/GameNodes/SGMover.m
@@ -18,17 +18,23 @@
     xDirection -= position_.x;
     yDirection -= position_.y;
     
+    if( isnan(xDirection) || isnan(yDirection) )
+        NSLog(@"Invalid position");
+    
     float magnitude = sqrtf(xDirection * xDirection + yDirection * yDirection);
     
     xDirection /= magnitude;
     yDirection /= magnitude;
     
-    float rotation = atanf((xDirection == 0) ? 0.0f : yDirection/xDirection);
+    float rotation = atan2f(yDirection,xDirection);
     
     [super setPosition:position];
     
     if( rotation != rotation_ )
-        [self setRotation:CC_RADIANS_TO_DEGREES(rotation)];
+    {
+        [self setRotation:CC_RADIANS_TO_DEGREES(rotation) - 90.0f];
+//        NSLog(@"Rotated to %f degress with x: %f y: %f", CC_RADIANS_TO_DEGREES(rotation), xDirection, yDirection);
+    }
 }
 
 @end
